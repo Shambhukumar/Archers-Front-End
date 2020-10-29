7659759bc7e7fb61468d2eae6fa74743a4594387
shambhu kumar <shambhukumar.shau@gmail.com>
2020-10-29 09:16:27 +0530

    changes redux dev tool

--- src/store/index.js
+++ src/store/index.js
@@ -3,11 +3,5 @@ import auth from "./reducer/auth";
 import thunk from "redux-thunk";
 const middleware = [thunk];
 const reducer = combineReducers({ user: auth });
-const store = createStore(
-  reducer,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = createStore(reducer,compose(applyMiddleware(...middleware)));
 export default store;
