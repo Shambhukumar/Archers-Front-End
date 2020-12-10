658edac380a5576511242a71db055209729a4b55
shambhu kumar <shambhukumar.shau@gmail.com>
2020-12-10 13:09:59 +0530

    some changes

--- src/store/index.js
+++ src/store/index.js
@@ -3,5 +3,5 @@ import auth from "./reducer/auth";
 import thunk from "redux-thunk";
 const middleware = [thunk];
 const reducer = combineReducers({ user: auth });
-const store = createStore(reducer,{},applyMiddleware(...middleware));
+const store = createStore(reducer,applyMiddleware(...middleware));
 export default store;
