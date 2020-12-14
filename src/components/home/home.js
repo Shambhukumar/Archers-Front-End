bf6f9bbb6291b1bee7cc0caaff619bd3b9f98baf
shambhu kumar <shambhukumar.shau@gmail.com>
2020-12-14 10:38:32 +0530

    Some changes

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -45,7 +45,7 @@ const Home = (props) => {
     
     if(token){
       const data = await axios.post(
-      "https://git.heroku.com/young-tundra-49076.git/"+process.env.REACT_APP_BASE_URL+"getdata",
+      "https://young-tundra-49076.herokuapp.com/"+process.env.REACT_APP_BASE_URL+"getdata",
         { date },
         {
           headers: {
